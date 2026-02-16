da085852e8f7ec549ac714bf2053e18bf86a039f
1nF0rmed <prkinformed@gmail.com>
2026-02-16 23:07:51 +0530

    Bump version to 0.2.0

--- simboba/__init__.py
+++ simboba/__init__.py
@@ -4,5 +4,5 @@ from simboba.boba import Boba
 from simboba.schemas import AgentResponse, MessageInput
 from simboba.prompts import JUDGE_PROMPT
 
-__version__ = "0.1.8"
+__version__ = "0.2.0"
 __all__ = ["Boba", "AgentResponse", "MessageInput", "JUDGE_PROMPT"]
